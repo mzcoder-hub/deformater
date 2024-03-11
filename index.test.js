eb73e58bc0a78f8c8f9de30ec765028acc971ebc
Galang Yoga Pradana <galangypradana@gmail.com>
2024-03-12 05:53:03 +0700

    include test file for better user or developer testing

---
+++ index.test.js
@@ -0,0 +1,7 @@
+// index.test.js
+const formatDate = require("./index");
+
+test("formats date correctly", () => {
+  const date = new Date("2024-03-12");
+  expect(formatDate(date, "UTC", "en-US")).toBe("12 March 2024");
+});
